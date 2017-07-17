ce2ad4e714c20e85f36b02a54c604e4916febd61
Hanishu <harisan78@gmail.com>
2017-07-18 00:11:37 +0530

    Thanks

---
+++ src/main/java/com/mkyong/common/HelloWorld1.java
@@ -0,0 +1,18 @@
+package com.mkyong.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld1 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hello entered again..pls reflectyes  java ! " + name);
+	}
+}
