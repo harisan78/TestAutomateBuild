161ed7fef058bc311bba10c59508c9f51fcde05b
Hanishu <harisan78@gmail.com>
2017-07-22 23:56:58 +0530

    Now test in Maven on Jenkins

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -14,7 +14,7 @@ public class App
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
     	 
-        HelloWorld obj = (HelloWorld) context.getBean("helloBean";
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
         
     }
